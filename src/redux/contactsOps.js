d0a3d932738b777d4cb775e868d70e06b7714262
TetiZ <tommytaba99@gmail.com>
2024-03-28 20:28:44 +0100

    https path correct

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,13 +1,18 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://6602bdf49d7276a755541afa.mockapi.io";
+axios.defaults.baseURL = "https://connections-api.herokuapp.com";
+
+// const setAuthHeader = (token) =>
+//   (axios.defaults.headers.common["Authorization"] = `Bearer ${token}`);
+// const clearAuthHeader = () =>
+//   (axios.defaults.headers.common["Authorization"] = "");
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get("./contacts");
+      const response = await axios.get("/contacts");
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -19,7 +24,7 @@ export const addContact = createAsyncThunk(
   "contacts/addContact",
   async (newContact, thunkAPI) => {
     try {
-      const response = await axios.post("./contacts", newContact);
+      const response = await axios.post("/contacts", newContact);
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -31,7 +36,7 @@ export const deleteContact = createAsyncThunk(
   "contacts/deleteContact",
   async (contactId, thunkAPI) => {
     try {
-      const response = await axios.delete(`./contacts/${contactId}`);
+      const response = await axios.delete(`/contacts/${contactId}`);
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
